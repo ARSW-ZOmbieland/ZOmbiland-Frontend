6d011b92bda812075c0e699b8fbfef3ce718eb31
tomasote572 <tomas.ramirez@mail.escuelaing.edu.co>
2026-05-14 21:46:53 -0500

    fix: configurar proxy para habilitar cookies same-site

--- src/config/constants.js
+++ src/config/constants.js
@@ -61,4 +61,5 @@ export const ALL_ASSETS = {
     ...PROP_ASSETS
 };
 
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+export const API_BASE_URL = ''; // Usar rutas relativas para el proxy de Azure
+
